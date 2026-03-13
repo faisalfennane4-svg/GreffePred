e8fed68d496df21386014dd0f6c214e859b9782b
sameabel09-alt <abel.same@centrale-casablanca.ma>
2026-03-13 09:43:24 +0000

    Update __init__.py

---
+++ app/__init__.py
@@ -0,0 +1,11 @@
+"""
+Package app
+
+Interface Streamlit pour la prédiction du succès
+des greffes de moelle osseuse pédiatriques.
+"""
+
+__version__ = "1.0.0"
+__author__ = "Abel Same"
+__institution__ = "Ecole Centrale Casablanca"
+__project__ = "Bone Marrow Transplant Prediction"
